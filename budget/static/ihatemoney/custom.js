a5cc50b0c245f55e15e8fc43feed90dbbc6ab771
Tinmn <royque@gmail.com>
2011-11-20 14:37:32 +0100

    Addition of an auto hiding user information message in the add user text field (with translation).

--- budget/static/ihatemoney/custom.js
+++ budget/static/ihatemoney/custom.js
@@ -15,3 +15,30 @@
         }
     }
 
+// Automatically hide and show the default value of a text field
+// handly in order to write user information in the text field.
+// jquery selector should return only one text field.
+    var auto_hide_default_text = function(text_field_selector){
+        // record the text in the text field before the first text field focus
+        var default_text;
+        
+        var hide_text = function(){
+            if(default_text==undefined){
+                default_text=this.value;
+                this.value="";
+            }
+            else if(this.value==default_text){
+                this.value="";
+            }
+        }
+
+        var show_text = function(){
+            if(this.value==""){
+                this.value=default_text;
+            }
+        }
+        
+        var field = $(text_field_selector);
+        field.focus(hide_text);
+        field.blur(show_text);
+    }
