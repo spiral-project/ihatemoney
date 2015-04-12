4f4439eec9c13a0424b3dc33304feda8756c0116
Alexandre Avenel <avenel.alexandre@gmail.com>
2015-04-12 20:41:57 +0200

    Fix #118 : Create a new project with the name "dashboard" should raise an exception

--- budget/forms.py
+++ budget/forms.py
@@ -88,7 +88,7 @@ class ProjectForm(EditProjectForm):
 
     def validate_id(form, field):
         form.id.data = slugify(field.data)
-        if Project.query.get(form.id.data):
+        if (form.id.data == "dashboard") or Project.query.get(form.id.data):
             raise ValidationError(Markup(_("The project identifier is used "
                 "to log in and for the URL of the project. "
                 "We tried to generate an identifier for you but a project "
