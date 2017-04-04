788c16c497bed6f455273575d5ca6b3598e97beb
Jocelyn Delalande <jocelyn@crapouillou.net>
2017-04-04 16:04:11 +0200

    Preparing release 0.9

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,7 +3,7 @@ Changelog
 
 This document describes changes between each past release.
 
-1.0.0 (2017-03-21)
-------------------
+0.9 (2017-04-04)
+----------------
 
 - First release of the project.
--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ ENTRY_POINTS = {
 
 
 setup(name='ihatemoney',
-      version='1.0.0',
+      version='0.9',
       description='A simple shared budget manager web application.',
       long_description="{}\n\n{}".format(README, CHANGELOG),
       license='Custom BSD Beerware',
