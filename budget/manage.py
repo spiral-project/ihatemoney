63db9222698f6a809a0814c9be19f077c777bd28
Alexis Métaireau <alexis@notmyidea.org>
2017-06-21 02:48:07 +0200

    Make all imports relative

--- budget/manage.py
+++ budget/manage.py
@@ -3,14 +3,16 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from run import app
-from models import db
+from .run import app
+from .models import db
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+def main():
+    manager.run()
 
 if __name__ == '__main__':
-    manager.run()
+    main()
