e4074ccaddc2f2843228f92072c92e1825ae6327
Jocelyn Delalande <jocelyn@crapouillou.net>
2017-10-23 09:49:08 +0200

    Add a command to generate configuration examples
    Config files are generated from templates (which remplace previous example files).
    
    - solve the issue of hard-to-explain configuration examples
    - ease pkg path seeking (avoid it, actually)
    - add working defaults for sqlite and unix socket paths (instead of
      /replace/me/path/example)
    - move settings comments from default_settings.py to ihatemoney.cfg.j2, as it is
      the one that will be facing user.

--- ihatemoney/default_settings.py
+++ ihatemoney/default_settings.py
@@ -1,37 +1,10 @@
-# You can find more information about what these settings mean in the
-# documentation, available online at
-# http://ihatemoney.readthedocs.io/en/latest/installation.html#configuration
-
-# Turn this on if you want to have more output on what's happening under the
-# hood.
-DEBUG = False
-
-# The database URI, reprensenting the type of database and how to connect to it.
-# Enter an absolute path here.
+# Verbose and documented settings are in conf-templates/ihatemoney.cfg.j2
+DEBUG = SQLACHEMY_ECHO = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/ihatemoney.db'
-SQLACHEMY_ECHO = DEBUG
-
-# Will likely become the default value in flask-sqlalchemy >=3 ; could be removed
-# then:
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# You need to change this secret key, otherwise bad things might happen to your
-# users.
 SECRET_KEY = "tralala"
-
-# A python tuple describing the name and email adress of the sender of the mails.
 MAIL_DEFAULT_SENDER = ("Budget manager", "budget@notmyidea.org")
-
-# If set to True, a demonstration project will be activated.
 ACTIVATE_DEMO_PROJECT = True
-
-# If not empty, the specified password must be entered to create new projects.
-# DO NOT enter the password in cleartext. Generate a password hash with
-# "ihatemoney generate_password_hash" instead.
 ADMIN_PASSWORD = ""
-
-# If set to True (default value) anyone can create a new project.
 ALLOW_PUBLIC_PROJECT_CREATION = True
-
-# If set to True, an administration dashboard is available.
 ACTIVATE_ADMIN_DASHBOARD = False
--- ihatemoney/manage.py
+++ ihatemoney/manage.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
+import os
+import pkgutil
+import random
 from getpass import getpass
-from flask_script import Manager, Command
+
+from flask_script import Manager, Command, Option
 from flask_migrate import Migrate, MigrateCommand
+from jinja2 import Template
 from werkzeug.security import generate_password_hash
 
 from ihatemoney.run import create_app
@@ -18,6 +23,38 @@ class GeneratePasswordHash(Command):
         print(generate_password_hash(password))
 
 
+class ConfigTemplate(Command):
+    def get_options(self):
+        return [
+            Option('config_file', choices=[
+                'ihatemoney.cfg',
+                'apache-vhost.conf',
+                'gunicorn.conf.py',
+                'supervisord.conf',
+                'nginx.conf',
+            ]),
+        ]
+
+    @staticmethod
+    def gen_secret_key():
+        return ''.join([
+            random.SystemRandom().choice(
+                'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
+            for i in range(50)])
+
+    def run(self, config_file):
+        template_content = pkgutil.get_data(
+            'ihatemoney',
+            os.path.join('conf-templates/', config_file) + '.j2'
+        ).decode('utf-8')
+
+        print(Template(template_content).render(
+                pkg_path=os.path.abspath(os.path.dirname(__file__)),
+                venv_path=os.environ.get('VIRTUAL_ENV'),
+                secret_key=self.gen_secret_key(),
+        ))
+
+
 def main():
     app = create_app()
     Migrate(app, db)
@@ -25,6 +62,7 @@ def main():
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
     manager.add_command('generate_password_hash', GeneratePasswordHash)
+    manager.add_command('generate-config', ConfigTemplate)
     manager.run()
 
 
