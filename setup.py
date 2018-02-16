d3d1ddfa9164a43695fc921d22c348e25256db66
Jocelyn Delalande <jocelyn@crapouillou.net>
2018-02-16 23:18:12 +0100

    Preparing release 2.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ ENTRY_POINTS = {
 
 
 setup(name='ihatemoney',
-      version='2.1.dev0',
+      version='2.1',
       description='A simple shared budget manager web application.',
       long_description="{}\n\n{}".format(README.encode('utf-8'), CHANGELOG.encode('utf-8')),
       license='Custom BSD Beerware',
