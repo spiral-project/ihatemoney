b68692621c8f42172173c87284b30b9b053aa19e
DavidRThrashJr <dthrash@zoho.com>
2020-02-07 23:55:01 -0500

    * Made the forms template more readable * Removed FIXME within edit_bill function, no bill will be returned if the bill_id is not associated with that particular project * Resolved FIXME within the pay_each function, now using a sql query for sum rather than for loop

--- ihatemoney/models.py
+++ ihatemoney/models.py
@@ -6,6 +6,7 @@ from flask import g, current_app
 
 from debts import settle
 from sqlalchemy import orm
+from sqlalchemy.sql import func
 from itsdangerous import (
     TimedJSONWebSignatureSerializer,
     URLSafeSerializer,
@@ -376,8 +377,10 @@ class Bill(db.Model):
     def pay_each(self):
         """Compute what each share has to pay"""
         if self.owers:
-            # FIXME: SQL might do that more efficiently
-            weights = sum(i.weight for i in self.owers)
+            weights = (db.session.query(func.sum(Person.weight))
+                       .join(billowers, Bill)
+                       .filter(Bill.id == self.id))\
+                .scalar()
             return self.amount / weights
         else:
             return 0
