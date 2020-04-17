114c151501ff70b0a697575763a2498d32532156
Andrew Dickinson <dickinsa@umich.edu>
2020-04-17 15:53:28 -0400

    Mark RegEx string as raw

--- ihatemoney/tests/tests.py
+++ ihatemoney/tests/tests.py
@@ -2250,7 +2250,7 @@ class ModelsTestCase(IhatemoneyTestCase):
 
 def em_surround(string, regex_escape=False):
     if regex_escape:
-        return '<em class="font-italic">%s<\/em>' % string
+        return r'<em class="font-italic">%s<\/em>' % string
     else:
         return '<em class="font-italic">%s</em>' % string
 
