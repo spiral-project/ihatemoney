5cb6d88468bd247b815e3c05e92b184e08dfe8a0
Adrien CLERC <adrien@antipoul.fr>
2020-06-15 22:58:22 +0200

    Back to development: 4.1.5

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ Changelog
 
 This document describes changes between each past release.
 
+4.1.5 (unreleased)
+==================
+
+- Nothing changed yet.
+
+
 4.1.4 (2020-06-07)
 ==================
 
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ ENTRY_POINTS = {
 
 
 setup(name='ihatemoney',
-      version='4.1.4',
+      version='4.1.5.dev0',
       description='A simple shared budget manager web application.',
       long_description=description,
       license='Custom BSD Beerware',
