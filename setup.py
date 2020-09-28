661939f0a1b14a87f0efe7c4c04a0400933b73b1
Azhar Mithani <25031629+AzharMithani@users.noreply.github.com>
2020-09-28 00:15:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+#importing the library
 from setuptools import setup
 
+#calling the main setup function
 setup()
+
+
+
+# might find code disclaimer template here https://estore.merge.com/mergecom3/resources/sourcecodedisclaimer.htm
