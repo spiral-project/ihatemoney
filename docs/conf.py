24fca575e5ee829e707ad8fd2958f8b72fa166d5
Alexis Métaireau <alexis@notmyidea.org>
2021-10-19 19:28:22 +0200

    Update documentation configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -3,9 +3,9 @@ source_suffix = ".rst"
 master_doc = "index"
 
 project = "I hate money"
-copyright = "2011, The 'I hate money' team"
+copyright = "2011-2021, The 'I hate money' team"
 
-version = "1.0"
-release = "1.0"
+version = "5.0"
+release = "5.0"
 exclude_patterns = ["_build"]
 pygments_style = "sphinx"
