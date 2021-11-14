f2d3ec1fc2a19ac72cbaec8913d35fcda949ea3a
Youe Graillot <youe.graillot@gmail.com>
2021-11-14 18:36:59 +0100

    /health endpoint

--- ihatemoney/web.py
+++ ihatemoney/web.py
@@ -151,6 +151,9 @@ def pull_project(endpoint, values):
             # redirect to authentication page
             raise Redirect303(url_for(".authenticate", project_id=project_id))
 
+@main.route("/health", methods=["GET"])
+def health():
+    return "OK"
 
 @main.route("/admin", methods=["GET", "POST"])
 def admin():
