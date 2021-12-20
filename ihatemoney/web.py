d60841e1bb915ccd193a6aa0a6b29146063fd787
Glandos <bugs-github@antipoul.fr>
2021-12-20 16:51:34 +0100

    improve comments and naming

--- ihatemoney/web.py
+++ ihatemoney/web.py
@@ -654,14 +654,16 @@ def list_bills():
     if "last_selected_payer" in session:
         bill_form.payer.data = session["last_selected_payer"]
 
-    # Each item will be (weight_sum, Bill) tuple
-    weights_bills = g.project.get_bill_weights_ordered().paginate(
+    # Each item will be (weight_sum, Bill) tuple.
+    # TODO: improve this awkward result using column_property:
+    # https://docs.sqlalchemy.org/en/14/orm/mapped_sql_expr.html.
+    weighted_bills = g.project.get_bill_weights_ordered().paginate(
         per_page=100, error_out=True
     )
 
     return render_template(
         "list_bills.html",
-        bills=weights_bills,
+        bills=weighted_bills,
         member_form=MemberForm(g.project),
         bill_form=bill_form,
         csrf_form=csrf_form,
