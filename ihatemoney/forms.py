cedb2934b856336ecbd0bce8dc598ec29afce23f
Zhongqi Ma <59981957+mzhongqi@users.noreply.github.com>
2022-12-10 18:46:58 -0500

    Updated checks for validate_name() in MemberForm()
    The database allows users to deactivate an account with a non-zero value, and create a new user with the same name, reactivating the previous user will allow two users of the same name. This change assures that new user names can not be the same as deactivated users with associated bills (Users that are not deleted from deactivation).

--- ihatemoney/forms.py
+++ ihatemoney/forms.py
@@ -457,7 +457,6 @@ class MemberForm(FlaskForm):
             and Person.query.filter(
                 Person.name == field.data,
                 Person.project == self.project,
-                Person.activated,
             ).all()
         ):  # NOQA
             raise ValidationError(_("This project already have this participant"))
