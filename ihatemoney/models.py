spiral-project/ihatemoney

commit 720f0e52ddbacf2a8ee4f1426978548f36b6cf77

Adding bill types and automatic settling between people (#1290)
* Bill types added in Bill and Project Model, Implemented in BillForm
* import and export bill feature updated with bill type, tests modified to reflect the behavior
* eliminating unnecessary bill type
* typo fixed, test cases fixed for the current bill types
* button added
* settle button added
* new changes
* test cases added
* bchen-reimbursement
* tests for different bill types
* test cases fixed
* fixed reimbursement test case
* Replaced assertEqual with assert
* Fixed missing bill_type in unit tests
* Removed commented code
* Reverted unnecessary string edit
* Changed bill_type to an Enum
* Added test checking correct bill_type validation
* Fixed  billtype displaying in all caps
* Removed 'Transfer' bill type
* Added migration rule and set default bill_type in alembic
* bill_type is now an optional parameter in the BillForm
* Use enum name instead of value as SQL server_default

SQLAlchemy uses the Enum names in the database, as the values could be
generic python objects.
https://docs.sqlalchemy.org/en/20/core/type_basics.html#sqlalchemy.types.Enum

* Removed bill type from the Bills html table
* Replaced string bill type with enum
* Made "Settlement" translatable
* Manually handle the new Enum creation

Alembic does not handle postgres Enums correctly, so we need to manually
generate the new enum type.
See https://github.com/sqlalchemy/alembic/issues/278

---------

Co-authored-by: Ruitao Li <ruital@andrew.cmu.edu>
Co-authored-by: MelodyZhangYiqun <98992024+MelodyZhangYiqun@users.noreply.github.com>
Co-authored-by: Ruitao Li <49292515+FlowingCloudRTL@users.noreply.github.com>
Co-authored-by: MelodyZhangYiqun <yiqunz@andrew.cmu.edu>
Co-authored-by: Brandan Chen <bychen@andrew.cmu.edu>
Co-authored-by: Emilie Zhou <54161959+ez157@users.noreply.github.com>
Co-authored-by: Tom <tom.roussel@esat.kuleuven.be>

ihatemoney/models.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from enum import Enum
 import datetime
 import itertools
 
@@ -21,7 +22,7 @@ from sqlalchemy_continuum.plugins import FlaskPlugin
 
 from ihatemoney.currency_convertor import CurrencyConverter
 from ihatemoney.monkeypath_continuum import PatchedTransactionFactory
-from ihatemoney.utils import generate_password_hash, get_members, same_bill
+from ihatemoney.utils import generate_password_hash, get_members, same_bill, FormEnum
 from ihatemoney.versioning import (
     ConditionalVersioningManager,
     LoggingMode,
@@ -49,6 +50,15 @@ make_versioned(
         )
     ],
 )
+
+class BillType(Enum):
+    EXPENSE = "Expense"
+    REIMBURSEMENT = "Reimbursement"
+
+    @classmethod
+    def choices(cls):
+        return [(choice, choice.value) for choice in cls]
+
 
 db = SQLAlchemy()
 
@@ -112,22 +122,31 @@ class Project(db.Model):
         - dict mapping each member to how much he/she should be paid by
           others (i.e. how much he/she has paid for bills)
 
+        balance       spent        paid
         """
         balances, should_pay, should_receive = (defaultdict(int) for time in (1, 2, 3))
-
         for bill in self.get_bills_unordered().all():
-            should_receive[bill.payer.id] += bill.converted_amount
             total_weight = sum(ower.weight for ower in bill.owers)
-            for ower in bill.owers:
-                should_pay[ower.id] += (
-                    ower.weight * bill.converted_amount / total_weight
-                )
+
+            if bill.bill_type == BillType.EXPENSE:
+                should_receive[bill.payer.id] += bill.converted_amount
+                for ower in bill.owers:
+                    should_pay[ower.id] += (ower.weight * bill.converted_amount / total_weight)
+
+            if bill.bill_type == BillType.REIMBURSEMENT:
+                should_receive[bill.payer.id] += bill.converted_amount
+                for ower in bill.owers:
+                    should_receive[ower.id] -= bill.converted_amount
 
         for person in self.members:
             balance = should_receive[person.id] - should_pay[person.id]
             balances[person.id] = balance
 
-        return balances, should_pay, should_receive
+        return (
+            balances,
+            should_pay,
+            should_receive,
+        )
 
     @property
     def balance(self):
@@ -160,7 +179,8 @@ class Project(db.Model):
         """
         monthly = defaultdict(lambda: defaultdict(float))
         for bill in self.get_bills_unordered().all():
-            monthly[bill.date.year][bill.date.month] += bill.converted_amount
+            if bill.bill_type == BillType.EXPENSE:
+                monthly[bill.date.year][bill.date.month] += bill.converted_amount
         return monthly
 
     @property
@@ -336,6 +356,7 @@ class Project(db.Model):
             pretty_bills.append(
                 {
                     "what": bill.what,
+                    "bill_type": bill.bill_type.value,
                     "amount": round(bill.amount, 2),
                     "currency": bill.original_currency,
                     "date": str(bill.date),
@@ -407,6 +428,7 @@ class Project(db.Model):
                     new_bill = Bill(
                         amount=b["amount"],
                         date=parse(b["date"]),
+                        bill_type=b["bill_type"],
                         external_link="",
                         original_currency=b["currency"],
                         owers=Person.query.get_by_names(b["owers"], self),
@@ -537,14 +559,15 @@ class Project(db.Model):
         db.session.commit()
 
         operations = (
-            ("Georg", 200, ("Amina", "Georg", "Alice"), "Food shopping"),
-            ("Alice", 20, ("Amina", "Alice"), "Beer !"),
-            ("Amina", 50, ("Amina", "Alice", "Georg"), "AMAP"),
+            ("Georg", 200, ("Amina", "Georg", "Alice"), "Food shopping", "Expense"),
+            ("Alice", 20, ("Amina", "Alice"), "Beer !", "Expense"),
+            ("Amina", 50, ("Amina", "Alice", "Georg"), "AMAP", "Expense"),
         )
-        for payer, amount, owers, what in operations:
+        for (payer, amount, owers, what, bill_type) in operations:
             db.session.add(
                 Bill(
                     amount=amount,
+                    bill_type=bill_type,
                     original_currency=project.default_currency,
                     owers=[members[name] for name in owers],
                     payer_id=members[payer].id,
@@ -677,6 +700,7 @@ class Bill(db.Model):
     date = db.Column(db.Date, default=datetime.datetime.now)
     creation_date = db.Column(db.Date, default=datetime.datetime.now)
     what = db.Column(db.UnicodeText)
+    bill_type = db.Column(db.Enum(BillType))
     external_link = db.Column(db.UnicodeText)
 
     original_currency = db.Column(db.String(3))
@@ -696,6 +720,7 @@ class Bill(db.Model):
         payer_id: int = None,
         project_default_currency: str = "",
         what: str = "",
+        bill_type: str = "Expense",
     ):
         super().__init__()
         self.amount = amount
@@ -705,6 +730,7 @@ class Bill(db.Model):
         self.owers = owers
         self.payer_id = payer_id
         self.what = what
+        self.bill_type = BillType(bill_type)
         self.converted_amount = self.currency_helper.exchange_currency(
             self.amount, self.original_currency, project_default_currency
         )
@@ -719,6 +745,7 @@ class Bill(db.Model):
             "date": self.date,
             "creation_date": self.creation_date,
             "what": self.what,
+            "bill_type": self.bill_type.value,
             "external_link": self.external_link,
             "original_currency": self.original_currency,
             "converted_amount": self.converted_amount,

ihatemoney/forms.py
@@ -39,7 +39,7 @@ from wtforms.validators import (
 )
 
 from ihatemoney.currency_convertor import CurrencyConverter
-from ihatemoney.models import Bill, LoggingMode, Person, Project
+from ihatemoney.models import Bill, BillType, LoggingMode, Person, Project
 from ihatemoney.utils import (
     em_surround,
     eval_arithmetic_expression,
@@ -364,6 +364,7 @@ class BillForm(FlaskForm):
     payed_for = SelectMultipleField(
         _("For whom?"), validators=[DataRequired()], coerce=int
     )
+    bill_type = SelectField(_("Bill Type"), choices=BillType.choices(), coerce=BillType, default=BillType.EXPENSE)
     submit = SubmitField(_("Submit"))
     submit2 = SubmitField(_("Submit and add a new one"))
 
@@ -377,12 +378,14 @@ class BillForm(FlaskForm):
             payer_id=self.payer.data,
             project_default_currency=project.default_currency,
             what=self.what.data,
+            bill_type=self.bill_type.data,
         )
 
     def save(self, bill, project):
         bill.payer_id = self.payer.data
         bill.amount = self.amount.data
         bill.what = self.what.data
+        bill.bill_type = BillType(self.bill_type.data)
         bill.external_link = self.external_link.data
         bill.date = self.date.data
         bill.owers = Person.query.get_by_ids(self.payed_for.data, project)
@@ -396,6 +399,7 @@ class BillForm(FlaskForm):
         self.payer.data = bill.payer_id
         self.amount.data = bill.amount
         self.what.data = bill.what
+        self.bill_type.data = bill.bill_type
         self.external_link.data = bill.external_link
         self.original_currency.data = bill.original_currency
         self.date.data = bill.date

ihatemoney/migrations/versions/7a9b38559992_new_bill_type_attribute_added.py
@@ -1,0 +1,31 @@
+"""new bill type attribute added
+
+Revision ID: 7a9b38559992
+Revises: 927ed575acbd
+Create Date: 2022-12-10 17:25:38.387643
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "7a9b38559992"
+down_revision = "927ed575acbd"
+
+from alembic import op
+import sqlalchemy as sa
+from ihatemoney.models import BillType
+
+
+def upgrade():
+    billtype_enum = sa.Enum(BillType)
+    billtype_enum.create(op.get_bind(), checkfirst=True)
+
+    op.add_column("bill", sa.Column("bill_type", billtype_enum, server_default=BillType.EXPENSE.name))
+    op.add_column("bill_version", sa.Column("bill_type", sa.UnicodeText()))
+
+
+def downgrade():
+    op.drop_column("bill", "bill_type")
+    op.drop_column("bill_version", "bill_type")
+
+    billtype_enum = sa.Enum(BillType)
+    billtype_enum.drop(op.get_bind())

ihatemoney/templates/forms.html
@@ -165,6 +165,7 @@
     {% include "display_errors.html" %}
     {{ form.hidden_tag() }}
     {{ input(form.date, inline=True) }}
+    {{ input(form.bill_type, inline=True) }}
     {{ input(form.what, inline=True) }}
     {{ input(form.payer, inline=True, class="form-control custom-select") }}
     <div data-toggle="tooltip" data-placement="top" title='{{ _("Simple operations are allowed, e.g. (18+36.2)/3") }}'>

ihatemoney/templates/settle_bills.html
@@ -9,13 +9,20 @@
 
 {% block content %}
     <table id="bill_table" class="split_bills table table-striped">
-        <thead><tr><th>{{ _("Who pays?") }}</th><th>{{ _("To whom?") }}</th><th>{{ _("How much?") }}</th></tr></thead>
+        <thead><tr><th>{{ _("Who pays?") }}</th><th>{{ _("To whom?") }}</th><th>{{ _("How much?") }}</th><th>{{ _("Settled?") }}</th></tr></thead>
     <tbody>
     {% for bill in bills %}
     <tr receiver={{bill.receiver.id}}>
             <td>{{ bill.ower }}</td>
             <td>{{ bill.receiver }}</td>
             <td>{{ bill.amount|currency }}</td>
+            <td>
+                <span id="settle-bill" class="ml-auto pb-2">
+                    <a href="{{ url_for('.settle', amount = bill.amount, ower_id = bill.ower.id, payer_id = bill.receiver.id) }}" class="btn btn-primary">
+                        {{ ("Settle") }}
+                    </a>
+                </span>
+            </td>
     </tr>
     {% endfor %}
     </tbody>

ihatemoney/tests/api_test.py
@@ -407,6 +407,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "25",
                 "external_link": "https://raclette.fr",
             },
@@ -431,6 +432,7 @@ class TestAPI(IhatemoneyTestCase):
                 {"activated": True, "id": 1, "name": "zorglub", "weight": 1},
                 {"activated": True, "id": 2, "name": "jeanne", "weight": 1},
             ],
+            "bill_type": "Expense",
             "amount": 25.0,
             "date": "2011-08-10",
             "id": 1,
@@ -462,6 +464,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "25",
                 "external_link": "https://raclette.fr",
             },
@@ -479,6 +482,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "beer",
                 "payer": "2",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "25",
                 "external_link": "https://raclette.fr",
             },
@@ -500,6 +504,7 @@ class TestAPI(IhatemoneyTestCase):
                 {"activated": True, "id": 1, "name": "zorglub", "weight": 1},
                 {"activated": True, "id": 2, "name": "jeanne", "weight": 1},
             ],
+            "bill_type": "Expense",
             "amount": 25.0,
             "date": "2011-09-10",
             "external_link": "https://raclette.fr",
@@ -554,6 +559,7 @@ class TestAPI(IhatemoneyTestCase):
                     "what": "fromage",
                     "payer": "1",
                     "payed_for": ["1", "2"],
+                    "bill_type": "Expense",
                     "amount": input_amount,
                 },
                 headers=self.get_auth("raclette"),
@@ -578,6 +584,7 @@ class TestAPI(IhatemoneyTestCase):
                     {"activated": True, "id": 1, "name": "zorglub", "weight": 1},
                     {"activated": True, "id": 2, "name": "jeanne", "weight": 1},
                 ],
+                "bill_type": "Expense",
                 "amount": expected_amount,
                 "date": "2011-08-10",
                 "id": id,
@@ -611,6 +618,7 @@ class TestAPI(IhatemoneyTestCase):
                     "what": "fromage",
                     "payer": "1",
                     "payed_for": ["1", "2"],
+                    "bill_type": "Expense",
                     "amount": amount,
                 },
                 headers=self.get_auth("raclette"),
@@ -658,6 +666,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "25",
                 "external_link": "https://raclette.fr",
             },
@@ -682,6 +691,7 @@ class TestAPI(IhatemoneyTestCase):
                 {"activated": True, "id": 1, "name": "zorglub", "weight": 1},
                 {"activated": True, "id": 2, "name": "jeanne", "weight": 1},
             ],
+            "bill_type": "Expense",
             "amount": 25.0,
             "date": "2011-08-10",
             "id": 1,
@@ -706,6 +716,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "30",
                 "external_link": "https://raclette.fr",
                 "original_currency": "CAD",
@@ -727,6 +738,7 @@ class TestAPI(IhatemoneyTestCase):
                 {"activated": True, "id": 1, "name": "zorglub", "weight": 1.0},
                 {"activated": True, "id": 2, "name": "jeanne", "weight": 1.0},
             ],
+            "bill_type": "Expense",
             "amount": 30.0,
             "date": "2011-08-10",
             "id": 1,
@@ -747,6 +759,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "Pierogi",
                 "payer": "1",
                 "payed_for": ["2", "3"],
+                "bill_type": "Expense",
                 "amount": "80",
                 "original_currency": "PLN",
             },
@@ -791,6 +804,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "25",
             },
             headers=self.get_auth("raclette"),
@@ -855,6 +869,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1", "2"],
+                "bill_type": "Expense",
                 "amount": "25",
             },
             headers=self.get_auth("raclette"),
@@ -877,6 +892,7 @@ class TestAPI(IhatemoneyTestCase):
                 {"activated": True, "id": 1, "name": "zorglub", "weight": 1},
                 {"activated": True, "id": 2, "name": "jeannedy familly", "weight": 4},
             ],
+            "bill_type": "Expense",
             "amount": 25.0,
             "date": "2011-08-10",
             "id": 1,
@@ -962,6 +978,7 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1"],
+                "bill_type": "Expense",
                 "amount": "0",
             },
             headers=self.get_auth("raclette"),
@@ -990,8 +1007,73 @@ class TestAPI(IhatemoneyTestCase):
                 "what": "fromage",
                 "payer": "1",
                 "payed_for": ["1"],
+                "bill_type": "Expense",
                 "amount": "9347242149381274732472348728748723473278472843.12",
             },
             headers=self.get_auth("raclette"),
         )
         self.assertStatus(400, req)
+
+    def test_validate_bill_type(self):
+        self.api_create("raclette")
+        self.api_add_member("raclette", "zorglub")
+        
+
+        req = self.client.post(
+            "/api/projects/raclette/bills",
+            data={
+                "date": "2011-08-10",
+                "what": "fromage",
+                "payer": "1",
+                "payed_for": ["1"],
+                "bill_type": "wrong_bill_type",
+                "amount": "50",
+            },
+            headers=self.get_auth("raclette")
+        )
+
+        self.assertStatus(400, req)
+
+        req = self.client.post(
+            "/api/projects/raclette/bills",
+            data={
+                "date": "2011-08-10",
+                "what": "fromage",
+                "payer": "1",
+                "payed_for": ["1"],
+                "bill_type": "Expense",
+                "amount": "50",
+            },
+            headers=self.get_auth("raclette")
+        )
+
+        self.assertStatus(201, req)
+
+    def test_default_bill_type(self):
+        self.api_create("raclette")
+        self.api_add_member("raclette", "zorglub")
+
+        # Post a bill without adding a bill type
+        req = self.client.post(
+            "/api/projects/raclette/bills",
+            data={
+                "date": "2011-08-10",
+                "what": "fromage",
+                "payer": "1",
+                "payed_for": ["1"],
+                "amount": "50",
+            },
+            headers=self.get_auth("raclette")
+        )
+
+        self.assertStatus(201, req)
+
+        req = self.client.get(
+            "/api/projects/raclette/bills/1", headers=self.get_auth("raclette")
+        )
+        self.assertStatus(200, req)
+
+        # Bill type should now be "Expense"
+        got = json.loads(req.data.decode("utf-8"))
+        assert got["bill_type"] == "Expense"
+        

ihatemoney/tests/history_test.py
@@ -212,6 +212,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "fromage à raclette",
                 "payer": user_id,
                 "payed_for": [user_id],
+                "bill_type": "Expense",
                 "amount": "25",
             },
             follow_redirects=True,
@@ -228,6 +229,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "fromage à raclette",
                 "payer": user_id,
                 "payed_for": [user_id],
+                "bill_type": "Expense",
                 "amount": "10",
             },
             follow_redirects=True,
@@ -371,6 +373,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "fromage à raclette",
                 "payer": 1,
                 "payed_for": [1],
+                "bill_type": "Expense",
                 "amount": "25",
             },
             follow_redirects=True,
@@ -391,6 +394,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "new thing",
                 "payer": 1,
                 "payed_for": [1],
+                "bill_type": "Expense",
                 "amount": "10",
             },
             follow_redirects=True,
@@ -477,6 +481,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "Bill 1",
                 "payer": 1,
                 "payed_for": [1, 2],
+                "bill_type": "Expense",
                 "amount": "25",
             },
         )
@@ -487,6 +492,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "Bill 2",
                 "payer": 1,
                 "payed_for": [1, 2],
+                "bill_type": "Expense",
                 "amount": "20",
             },
         )
@@ -505,6 +511,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "Bill 1",
                 "payer": 1,
                 "payed_for": [1, 2],
+                "bill_type": "Expense",
                 "amount": "88",
             },
         )
@@ -545,6 +552,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "Bill 1",
                 "payer": 1,
                 "payed_for": [1, 2],
+                "bill_type": "Expense",
                 "amount": "25",
             },
         )
@@ -567,6 +575,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "Bill 2",
                 "payer": 1,
                 "payed_for": [1, 2],
+                "bill_type": "Expense",
                 "amount": "20",
             },
         )
@@ -627,6 +636,7 @@ class TestHistory(IhatemoneyTestCase):
                 "what": "fromage à raclette",
                 "payer": 1,
                 "payed_for": [1],
+                "bill_type": "Expense",
                 "amount": "10",
                 "original_currency": "EUR",
             },

commit 2f1cdbbd22acf74764ceb38baace2598d584f0e7

fix: 'Bill Type: Invalid Choice: could not coerce' error
Error introduced in #1290. Fixes #1293. WTForms needs to be bumped to >=2.3.2
as it includes a fix to `SelectField` which is required for this change to work.

See:
  - https://wtforms.readthedocs.io/en/3.1.x/changes/#version-2-3-2
  - https://github.com/wtforms/wtforms/pull/598

ihatemoney/models.py
@@ -58,7 +58,7 @@ class BillType(Enum):
 
     @classmethod
     def choices(cls):
-        return [(choice, choice.value) for choice in cls]
+        return [(choice.value, choice.value) for choice in cls]
 
 
 db = SQLAlchemy()

pyproject.toml
@@ -39,7 +39,7 @@ dependencies = [
     "Flask-SQLAlchemy>=2.4,<3",
     "Flask-Talisman>=0.8,<2",
     "Flask-WTF>=0.14.3,<2",
-    "WTForms>=2.3.1,<3.2",
+    "WTForms>=2.3.3,<3.2",
     "Flask>=2,<3",
     "Werkzeug>=2,<3",
     "itsdangerous>=2,<3",